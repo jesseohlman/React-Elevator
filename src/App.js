880fe57a47607b470c75143a0a7da112177fef2e
jesseohlman <44782018+jesseohlman@users.noreply.github.com>
2019-09-23 16:33:56 -0400

    added functionality for elevator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentFloor: 1,
+      targetFloor: [],
+      doors: false,
+    }
+
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.changingFloors = this.changingFloors.bind(this);
+    this.timer = null;
+  }
+
+  changingFloors(){
+    var target = this.state.targetFloor;
+    var current = this.state.currentFloor;
+
+    if(target.includes(current)){
+      this.setState({doors: true});
+      target.splice(target.indexOf(current), 1);
+      clearInterval(this.timer); //stops floor movment while doors open
+      setTimeout(() => {
+          this.setState({doors: false, targetFloor: target});
+          this.timer = setInterval(this.changingFloors, 1000); //continues floor movement
+      }, 5000);
+      return null;
+    }
+
+    if( target[0] !== current){
+      if(target[0] > current){
+        this.setState({currentFloor: current + 1});
+      } else if(target[0] < current) {
+        this.setState({currentFloor: current - 1});
+      } else {
+        clearInterval(this.timer);
+        this.timer = null;
+      }
+    } else {
+      target.splice(0, 1);
+      this.setState({target: target});
+    }
+  }
+
+  handleButtonClick(e, floor){
+    e.preventDefault();
+    if(!this.state.targetFloor.includes(floor)){
+      var target = this.state.targetFloor;
+      target.push(floor)
+      this.setState({targetFloor: target, moving: true});
+    }
+
+      if(this.timer === null){
+        this.timer = setInterval(this.changingFloors, 1000);
+      }
+    
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>elevator</h1>
+        </header>
+        <div>current: {this.state.currentFloor} target: {this.state.targetFloor[0]}</div>
+        <div>doors: {this.state.doors ? "open" : "closed"}</div>
+        <form onSubmit={this.handleFloorChange}>
+          <button type="submit" className="btn btn-primary" onClick={(e) => this.handleButtonClick(e, 1)}>1</button>
+          <button type="submit" className="btn btn-primary" onClick={(e) => this.handleButtonClick(e, 2)}>2</button>
+          <button type="submit" className="btn btn-primary" onClick={(e) => this.handleButtonClick(e, 3)}>3</button>
+          <button type="submit" className="btn btn-primary" onClick={(e) => this.handleButtonClick(e, 4)}>4</button>
+          <button type="submit" className="btn btn-primary" onClick={(e) => this.handleButtonClick(e, 5)}>5</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
